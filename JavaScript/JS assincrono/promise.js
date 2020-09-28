13637473eb3e6fb98e4b2589ca45a2b886150bb7
Duarte <rafaella.d.d.carvalho@gmail.com>
2020-09-28 11:11:29 -0300

    Commit da atividade de axios

---
+++ JavaScript/JS assincrono/promise.js
@@ -0,0 +1,28 @@
+var minhaPromise = function() {
+    return new Promise(function (resolve, reject) {
+        var nhr = new XMLHttpRequest();
+        xhr.open('GET','https://api.github.com/users/elladarte');
+        xhr.send(null);
+
+        xhr.onreadystatechange =  function(){
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject('Erro na requisicao');
+                }
+            }
+        }
+    });
+}
+
+// var resultado = minhaPromise();
+// console.log(resultado);
+
+minhaPromise()
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(error) {
+        console.warn(error);
+    });
